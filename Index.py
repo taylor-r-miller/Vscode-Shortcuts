2a0a7e5f5568beef10eba5c8201960903b97c39c
taylor-r-miller <64097840+taylor-r-miller@users.noreply.github.com>
2020-11-22 18:58:04 -0600

    fixed typo in var change

--- Index.py
+++ Index.py
@@ -12,10 +12,10 @@ SHORTCUTS = {
         "include_all":True 
         },
     "work":{ #example of only listing certain files
-        "path": 'Documents/Work/',
+        "path": 'Documents/Work',
         "include_all": False,
         "include":[ #exact dir name
-  
+ 
         ]
     }
 }
@@ -41,7 +41,7 @@ def Shortcuts():
         osx_pattern = re.compile('^(/Users/.+?)/')
         full_path  = os.getcwd()
         base_path = f'{osx_pattern.search(full_path).group()}/'
-        abs_path = f'{base_pathss}{path}'
+        abs_path = f'{base_path}{path}'
         
     inlcude_all = info.get('include_all')
 
